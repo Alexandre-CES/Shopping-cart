ad7a6ea412525aa057093f17b088aecaada4857e
Alexandre <alexandreces666@gmail.com>
2025-06-11 21:20:46 -0300

    feat: fetching data from fakestoreAPI

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Product } from './Types/Product';
 
 function App() {
+  const [products,setProducts] = useState<Product[]>([]);
+
+  useEffect(()=>{
+    async function loadProducts(){
+      const productsData = await fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+      .catch((err)=>console.log('Error fething data: '+err))
+
+      setProducts(productsData);
+    }
+
+    loadProducts();
+  },[]);
+
   return (
-    <div className="App">
-      Hello, World!
-    </div>
+    <main>
+
+
+      <ul>
+      {
+        /*   
+        products.map((product)=>{
+          return(
+            <li key={product.id}>
+              <div>
+                {product.id};
+              </div>
+              <div>
+                {product.price};
+              </div>
+              <div>
+                {product.description};
+              </div>
+              <div>
+                {product.category};
+              </div>
+              <div>
+                {product.image};
+              </div>
+            </li>
+          );
+        })
+        */ 
+      }
+      </ul>
+    </main>
   );
 }
 
