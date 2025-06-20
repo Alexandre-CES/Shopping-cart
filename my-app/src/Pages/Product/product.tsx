7f0d52606525985b4a4a3bd393748e4dda086629
Alexandre <alexandreces666@gmail.com>
2025-06-19 22:02:03 -0300

    feat: get product by id

--- my-app/src/Pages/Product/product.tsx
+++ my-app/src/Pages/Product/product.tsx
@@ -1,10 +1,32 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Product } from "../../Types/Product";
 
-export default function Product(){
+export default function ProductView(){
     const { id } = useParams();
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(()=>{
+            async function loadProducts(){
+                const productData = await fetch(`https://fakestoreapi.com/products/${id}`)
+                .then(res=>res.json())
+                .catch((err)=>console.log('Error fething data: '+err))
+    
+                setProduct(productData);
+            }
+    
+            loadProducts();
+    },[]);
+
     return(
         <main>
-            {id}
+            
+            {product?.id}
+            {product?.title}
+            {product?.price}
+            
+            <img src={product?.image}/>
+
         </main>
     );
 }
--- my-app/src/Routes/routes.tsx
+++ my-app/src/Routes/routes.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../Pages/Home/home';
-import Product from "../Pages/Product/product";
+import ProductView from "../Pages/Product/product";
 
 export default function RoutesApp(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="product/:id" element={<Product/>}/>
+                <Route path="product/:id" element={<ProductView/>}/>
             </Routes>
         </BrowserRouter>
     )
