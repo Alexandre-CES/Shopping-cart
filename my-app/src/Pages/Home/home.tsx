a62f725ea28a26a2798850cb5c3859621afe9002
Alexandre <alexandreces666@gmail.com>
2025-06-23 16:13:42 -0300

    feat: store product in localStorage

--- my-app/src/Pages/Home/home.tsx
+++ my-app/src/Pages/Home/home.tsx
@@ -4,20 +4,24 @@ import * as Icon from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 
 export default function Home(){
-    const [products,setProducts] = useState<Product[]>([]);
+  const [products,setProducts] = useState<Product[]>([]);
 
-    //fetch data and load products everytime page is loaded
-    useEffect(()=>{
-        async function loadProducts(){
-            const productsData = await fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .catch((err)=>console.log('Error fething data: '+err))
+  //fetch data and load products everytime page is loaded
+  useEffect(()=>{
+      async function loadProducts(){
+          const productsData = await fetch('https://fakestoreapi.com/products')
+          .then(res=>res.json())
+          .catch((err)=>console.log('Error fething data: '+err))
 
-            setProducts(productsData);
-        }
+          setProducts(productsData);
+      }
 
-        loadProducts();
-    },[]);
+      loadProducts();
+  },[]);
+
+  function addToCart(product:Product):void{
+    localStorage.setItem(product?.id.toString(),JSON.stringify(product));
+  }
 
   return (
     <div className='container mt-5'>
@@ -30,7 +34,7 @@ export default function Home(){
             {
               products.map((product)=>{
                 return(
-                  <div className='col-md-4 mb-4'>
+                  <div key={product.id} className='col-md-4 mb-4'>
                       <div className='card'>
                           
                           <img className='img-fixed-size img-fluid w-100 img-thumbnail rounded' src={product.image}/>
@@ -39,7 +43,7 @@ export default function Home(){
                               <h3 className='card-title'>{product.price}$</h3>
                               <p className="card-text">{product.title}</p>
                               <Link to={'/product/'+product.id} target='_blank' className="card-link text-secondary">More</Link>
-                              <button className='btn btn-primary mx-2'><Icon.Cart2/></button>
+                              <button className='btn btn-primary mx-2' onClick={()=>addToCart(product)}><Icon.Cart2/></button>
                           </div>
                       </div>
                   </div>
